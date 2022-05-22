ee525ef930278f5fd3d5f605382303a3ab544c7b
kauecaires02 <84874622+kauecaires02@users.noreply.github.com>
2022-05-22 10:36:49 -0300

    Create dolar.py

---
+++ dolar.py
@@ -0,0 +1,19 @@
+# WebScrapping-dolar
+
+import requests
+
+from bs4 import BeautifulSoup       
+
+  headers = {'User-Agent':'Mozila/5.0 Windows NT 10.0;WOW64;Trident/7.0; rv:11.0)'}
+
+     page = requests.get("https://dolarhoje.com/"headers=header) #Acessar site
+
+ print(page.content)
+
+   soup = BeautifulSoup(page.content, 'html.parser')
+
+     atributos = {'class': 'id=cotação'}
+        
+        respostas = soup.find_all("div", attrs=atributos)
+  
+  print(Valor_dólar.text)
